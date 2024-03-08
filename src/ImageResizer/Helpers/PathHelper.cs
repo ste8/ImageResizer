6b530da54262ae8001b721bc21e714b10dbee007
stefano.ottaviani <stefano@smartmobile.it>
2024-03-08 18:31:23 +0100

    Fixed Path.GetAbsolutePath

--- src/ImageResizer/Helpers/PathHelper.cs
+++ src/ImageResizer/Helpers/PathHelper.cs
@@ -4,11 +4,12 @@
     {
         public static string GetAbsolutePath(string relativePath)
         {
-            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            string? assemblyPath = Path.GetDirectoryName(assembly.Location);
-            if (assemblyPath == null) {
-                throw new Exception("Cannot find assembly path.");
-            }
+            //var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            //string? assemblyPath = Path.GetDirectoryName(assembly.Location);
+            //if (assemblyPath == null) {
+            //    throw new Exception("Cannot find assembly path.");
+            //}
+            var assemblyPath = AppContext.BaseDirectory;
             var absolutePath = Path.Combine(assemblyPath, relativePath);
             return absolutePath;
         }
